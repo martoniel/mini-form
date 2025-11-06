4cd0f846f2ab1d3f44508d68e103b33dc50ab4e2
Kode <108406480+KodeVoid@users.noreply.github.com>
2025-11-06 17:51:43 +0100

    Fix function calls and improve message display

--- kTG Clone/ass.js
+++ kTG Clone/ass.js
@@ -1,22 +1,23 @@
 function showMessage() {
-    const firstName = document.getElementsByClassName("firstname").value;
-    const surName = document.getElementsByClassName("surname").value;
-    const age = document.getElementsByClassName("age").value;
-    const gender = document.getElementsByClassName("gender").value;    
+    const firstName = document.getElementByClassName("firstname").value;
+    const surName = document.getElementByClassName("surname").value;
+    const age = document.getElementByClassName("age").value;
+    const gender = document.getElementByClassName("gender").value;    
 
 
 const Message = 'HI, ${firstName} ${surName}, ${age} years old, ${gender}, welcome to our site!';
 
-document.getElementsByClassName("message").innerText = Message + "\nIf you want to continue click YES, if not click NO.";
+document.getElementByClassName("message").innerText = Message + "\nIf you want to continue click YES, if not click NO.";
 
-document.getElementsById("yes").style.display = "inline-block";
-document.getElementsById("no").style.display = "inline-block";}
+document.getElementById("yes").style.display = "inline-block";
+document.getElementById("no").style.display = "inline-block";}
 
 function continueSite() {
     alert("Great! Enjoy your stay.");}
 
 function exitSite(){
     alert("Goodbye! Enjoy Have a nice day.");}
+
 
 
 
